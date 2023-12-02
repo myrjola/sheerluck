f0bf6db16757838d98659fd7074db7e75bb86467
Martin Yrjölä <martin.yrjola@gmail.com>
2023-12-02 12:20:54 +0200

    feat: start scaffolding the navigation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,7 +19,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// the http.Error() function to send a generic 500 Internal Server Error
 	// response to the user. Note that we use the net/http constant
 	// http.StatusInternalServerError here instead of the integer 500 directly.
-	ts, err := template.ParseFiles("./ui/html/base.gohtml")
+	ts, err := template.ParseFiles("./ui/html/base.gohtml", "./ui/html/nav.gohtml")
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, internalServerError, http.StatusInternalServerError)
@@ -29,13 +29,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// Detect htmx header and render partial template
 	if r.Header.Get("Hx-Request") == "true" {
 		err = ts.ExecuteTemplate(w, "button", nil)
-		return
 	} else {
 		// Then we use the Execute() method on the template set to write the
 		// template content as the response body. The last parameter to Execute()
 		// represents any dynamic data that we want to pass in, which for now we'll
 		// leave as nil.
-		err = ts.Execute(w, nil)
+		err = ts.ExecuteTemplate(w, "base", nil)
 	}
 
 	if err != nil {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './ui/html/**/*.{html,js,gohtml}',
-  ],
+  content: ["./ui/html/**/*.{html,js,gohtml}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
