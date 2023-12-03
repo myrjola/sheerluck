5de9c272178177ab1326419253ae8f146e809718
Martin Yrjölä <martin.yrjola@gmail.com>
2023-12-03 09:25:28 +0200

    feat: extract routes to own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -32,32 +32,9 @@ func main() {
 		logger: logger,
 	}
 
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the questionPeople function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/question-people", app.questionPeople)
-	mux.HandleFunc("/question-people/stream", app.streamChat)
-	mux.HandleFunc("/investigate-scenes", app.investigateScenes)
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the Href given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Print a log a message to say that the server is starting.
 	logger.Info("starting server", slog.Any("addr", ":4000"))
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-	err = http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
