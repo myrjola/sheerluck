99ee5c8b8e27253c3195406a309b4b1e742569bf
Martin Yrjölä <martin.yrjola@gmail.com>
2023-12-28 22:03:46 +0200

    Implement ChannelBroker

---
+++ internal/broker/channelbroker.go
@@ -0,0 +1,109 @@
+package broker
+
+type publishChannelContent[TID comparable, TPayload any] struct {
+	ID      TID
+	Channel chan TPayload
+}
+
+type subscribeChannelContent[TID comparable, TPayload any] struct {
+	ID      TID
+	Channel chan chan TPayload
+}
+
+// ChannelBroker passes a channel with ID from producer to the first consumer.
+// The subsequent consumers will block until producer is finished so that they
+// can resolve the situation e.g. by fetching persisted data from the database.
+//
+// This kind of broker is useful for streaming chat responses through SSE. The
+// Producer in this case is a goroutine spawned by HTTP POST to initiate the streaming.
+// The first consumer is the HTTP handler that returns the SSE stream. The subsequent
+// consumers are likely caused by connectivity issues. In their case, it's better to
+// wait for the producer to finish and return the complete data at the end.
+type ChannelBroker[TID comparable, TPayload any] struct {
+	stopChannel      chan struct{}
+	publishChannel   chan publishChannelContent[TID, TPayload]
+	unpublishChannel chan TID
+	subscribeChannel chan subscribeChannelContent[TID, TPayload]
+}
+
+// NewChannelBroker creates a new ChannelBroker and starts the goroutine that handles it.
+// Use Stop() to stop the goroutine.
+func NewChannelBroker[TID comparable, TPayload any]() *ChannelBroker[TID, TPayload] {
+	broker := ChannelBroker[TID, TPayload]{
+		stopChannel:      make(chan struct{}),
+		publishChannel:   make(chan publishChannelContent[TID, TPayload]),
+		unpublishChannel: make(chan TID),
+		subscribeChannel: make(chan subscribeChannelContent[TID, TPayload]),
+	}
+	go broker.start()
+	return &broker
+}
+
+func (b *ChannelBroker[TID, TPayload]) start() {
+	publishedChannels := map[TID]chan TPayload{}
+	subscriberLists := map[TID][]chan chan TPayload{}
+	for {
+		select {
+		case <-b.stopChannel:
+			return
+
+		case subscription := <-b.subscribeChannel:
+			c := publishedChannels[subscription.ID]
+			if c == nil {
+				// Signal to the subscriber that the producer is finished (or haven't started yet)
+				close(subscription.Channel)
+				break
+			}
+			subscribers := subscriberLists[subscription.ID]
+			if subscribers == nil {
+				// First subscriber gets the channel from the producer
+				subscribers = []chan chan TPayload{subscription.Channel}
+				subscription.Channel <- c
+			} else {
+				// Subsequent subscribers block until the producer is finished
+				subscriberLists[subscription.ID] = append(subscribers, subscription.Channel)
+			}
+
+		case publication := <-b.publishChannel:
+			publishedChannels[publication.ID] = publication.Channel
+
+		case id := <-b.unpublishChannel:
+			delete(publishedChannels, id)
+			delete(subscriberLists, id)
+		}
+	}
+}
+
+// Stop the goroutine that handles the broker.
+func (b *ChannelBroker[TID, TPayload]) Stop() {
+	close(b.stopChannel)
+}
+
+// Subscribe to the channel with ID. Returns a channel that will receive the channel corresponding to the ID.
+// If the channel is not yet published, the returned channel will be closed.
+// If there's already a subscriber, the returned channel will block until the producer is finished and then
+// close the returned channel.
+func (b *ChannelBroker[TID, TPayload]) Subscribe(id TID) chan chan TPayload {
+	channel := make(chan chan TPayload, 1)
+	b.subscribeChannel <- subscribeChannelContent[TID, TPayload]{
+		ID:      id,
+		Channel: channel,
+	}
+	return channel
+}
+
+// Publish the channel with ID. The channel will be sent to the first subscriber.
+func (b *ChannelBroker[TID, TPayload]) Publish(id TID, channel chan TPayload) {
+	b.publishChannel <- publishChannelContent[TID, TPayload]{
+		ID:      id,
+		Channel: channel,
+	}
+}
+
+// Unpublish the channel with ID. Note that the channel will be removed from the broker which means
+// that subscribers will not be able to receive the channel from the broker. The suggested way to
+// get around this is an unbuffered channel that blocks the producer until it gets a consumer. If the
+// consumers are unreliable, the producer should have a timeout to not block forever.
+func (b *ChannelBroker[TID, TPayload]) Unpublish(id TID) {
+	b.unpublishChannel <- id
+}
---
+++ internal/broker/channelbroker_test.go
@@ -0,0 +1,78 @@
+package broker
+
+import (
+	"github.com/stretchr/testify/require"
+	"sync/atomic"
+	"testing"
+)
+
+func TestChannelBroker(t *testing.T) {
+	type testCase struct {
+		name     string
+		testFunc func(b *ChannelBroker[int, string])
+	}
+	tests := []testCase{
+		{
+			name: "subscriber receives content",
+			testFunc: func(b *ChannelBroker[int, string]) {
+				id := 1
+				channel := make(chan string)
+				b.Publish(id, channel)
+				go func() {
+					channel <- "hello"
+					close(channel)
+					b.Unpublish(id)
+				}()
+				subscriptionChan := <-b.Subscribe(id)
+				require.Equal(t, "hello", <-subscriptionChan, "subscriber did not receive content")
+				msg, ok := <-subscriptionChan
+				require.Empty(t, msg, "subscriber received content after producer closed")
+				require.Falsef(t, ok, "channel not closed")
+			},
+		},
+		{
+			name: "subsequent subscribers block until producer is finished or unpublished",
+			testFunc: func(b *ChannelBroker[int, string]) {
+				id := 1
+				channel := make(chan string)
+				b.Publish(id, channel)
+				producerFinished := atomic.Bool{}
+
+				// First subscriber
+				subscriptionChan := <-b.Subscribe(id)
+
+				// Next subscriber
+				go func() {
+					nextSubscriptionChan, ok := <-b.Subscribe(id)
+					require.Nil(t, nextSubscriptionChan, "subsequent subscriber received content")
+					require.Falsef(t, ok, "channel not closed to signal producer is finished")
+					require.True(t, producerFinished.Load(), "producer not finished before subsequent subscriber unblocked")
+				}()
+
+				// Finish producer
+				go func() {
+					channel <- "hello"
+					close(channel)
+					producerFinished.Store(true)
+					b.Unpublish(id)
+				}()
+				require.Equal(t, "hello", <-subscriptionChan, "subscriber did not receive content")
+
+				// Last subscriber
+				nextSubscriptionChan, ok := <-b.Subscribe(id)
+				require.Nil(t, nextSubscriptionChan, "last subscriber received content")
+				require.Falsef(t, ok, "last subscriber channel not closed to signal producer is finished")
+				require.True(t, producerFinished.Load(), "producer not finished before last subscriber unblocked")
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			broker := NewChannelBroker[int, string]()
+			t.Cleanup(func() {
+				broker.Stop()
+			})
+			tt.testFunc(broker)
+		})
+	}
+}
