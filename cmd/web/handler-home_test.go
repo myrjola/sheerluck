4d3ff70fa674b2abdff57f51c54f82b67a618594
Martin Yrjölä <martin.yrjola@gmail.com>
2024-12-26 16:16:42 +0200

    add testserver convenience

--- cmd/web/handler-home_test.go
+++ cmd/web/handler-home_test.go
@@ -1,160 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/descope/virtualwebauthn"
-	"github.com/justinas/nosurf"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"io"
-	"net/http"
-	url2 "net/url"
-	"strings"
 	"testing"
 )
 
 func Test_application_home(t *testing.T) {
-	url := startTestServer(t, io.Discard, testLookupEnv)
-	jar, err := newUnsafeCookieJar()
-	require.NoError(t, err)
-	client := &http.Client{Jar: jar}
-
-	res, err := client.Get(url)
-	require.NoError(t, err)
-	defer func(Body io.ReadCloser) {
-		err = Body.Close()
-		assert.NoError(t, err)
-	}(res.Body)
-
-	require.Equal(t, http.StatusOK, res.StatusCode)
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	require.NoError(t, err)
-
+	server := startTestServer(t, io.Discard, testLookupEnv)
+	doc := server.GetDoc(t, "/")
 	require.Equal(t, 1, doc.Find("button:contains('Sign in')").Length())
 	require.Equal(t, 1, doc.Find("button:contains('Register')").Length())
+	require.Equal(t, 0, doc.Find("button:contains('Log out')").Length())
 
-	rp := virtualwebauthn.RelyingParty{Name: "Sheerluck", ID: "localhost", Origin: "http://localhost:0"}
-	authenticator := virtualwebauthn.NewAuthenticator()
-	credential := virtualwebauthn.NewCredential(virtualwebauthn.KeyTypeEC2)
-
-	// Find the form
-	registrationStartURLPath := "/api/registration/start"
-	formSelector := fmt.Sprintf("form[action='%s']", registrationStartURLPath)
-	form := doc.Find(formSelector)
-	// Find the CSRF token
-	csrfToken, ok := form.Find("input[name=csrf_token]").Attr("value")
-	require.True(t, ok, "csrf_token not found in form %s", formSelector)
-
-	req, err := http.NewRequest(http.MethodPost, url+registrationStartURLPath, nil)
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set(nosurf.HeaderName, csrfToken)
-	resp, err := client.Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	bodyBytes, err := io.ReadAll(resp.Body)
-	err = resp.Body.Close()
-	require.NoError(t, err)
-	attOpts, err := virtualwebauthn.ParseAttestationOptions(string(bodyBytes))
-	require.NoError(t, err)
-	attestationResponse := virtualwebauthn.CreateAttestationResponse(rp, authenticator, credential, *attOpts)
-	req, err = http.NewRequest(http.MethodPost, url+"/api/registration/finish", strings.NewReader(attestationResponse))
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set(nosurf.HeaderName, csrfToken)
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-
-	// At this point, our credential is ready for logging in.
-	authenticator.AddCredential(credential)
-	// This option is needed for making Passkey login work.
-	authenticator.Options.UserHandle = []byte(attOpts.UserID)
-
-	res, err = client.Get(url)
-	require.NoError(t, err)
-	defer func(Body io.ReadCloser) {
-		err = Body.Close()
-		assert.NoError(t, err)
-	}(res.Body)
-
-	require.Equal(t, http.StatusOK, res.StatusCode)
-
-	doc, err = goquery.NewDocumentFromReader(res.Body)
-	require.NoError(t, err)
+	doc = server.Register(t)
+	require.Equal(t, 0, doc.Find("button:contains('Sign in')").Length())
+	require.Equal(t, 0, doc.Find("button:contains('Register')").Length())
 	require.Equal(t, 1, doc.Find("button:contains('Log out')").Length())
 
 	// Log out and log back in
-	doc = submitForm(t, client, url, "/api/logout", doc)
+	doc = server.SubmitForm(t, "/", "/api/logout")
+	require.Equal(t, 1, doc.Find("button:contains('Sign in')").Length())
+	require.Equal(t, 1, doc.Find("button:contains('Register')").Length())
+	require.Equal(t, 0, doc.Find("button:contains('Log out')").Length())
 
-	// Find the form
-	loginStartURLPath := "/api/login/start"
-	formSelector = fmt.Sprintf("form[action='%s']", loginStartURLPath)
-	form = doc.Find(formSelector)
-	// Find the CSRF token
-	csrfToken, ok = form.Find("input[name=csrf_token]").Attr("value")
-	require.True(t, ok, "csrf_token not found in form %s", formSelector)
-
-	req, err = http.NewRequest(http.MethodPost, url+loginStartURLPath, nil)
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set(nosurf.HeaderName, csrfToken)
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	bodyBytes, err = io.ReadAll(resp.Body)
-	err = resp.Body.Close()
-	require.NoError(t, err)
-	asOpts, err := virtualwebauthn.ParseAssertionOptions(string(bodyBytes))
-
-	require.NoError(t, err)
-	asResp := virtualwebauthn.CreateAssertionResponse(rp, authenticator, credential, *asOpts)
-	require.NoError(t, err)
-	req, err = http.NewRequest(http.MethodPost, url+"/api/login/finish", strings.NewReader(asResp))
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set(nosurf.HeaderName, csrfToken)
-	resp, err = client.Do(req)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
+	doc = server.Login(t)
+	require.Equal(t, 0, doc.Find("button:contains('Sign in')").Length())
+	require.Equal(t, 0, doc.Find("button:contains('Register')").Length())
+	require.Equal(t, 1, doc.Find("button:contains('Log out')").Length())
 	// TODOs for nicer test setup:
 	// - http client with
 	//     - html assertion helper
 	//     - form submission helper
 	// - webauthn glue for setting up users for tests
-}
-
-func submitForm(t *testing.T, client *http.Client, baseURL, url string, doc *goquery.Document) *goquery.Document {
-	html, err := doc.Html()
-	require.NoError(t, err)
-
-	// Find the form
-	formSelector := fmt.Sprintf("form[action='%s']", url)
-	form := doc.Find(formSelector)
-	require.Equal(t, 1, form.Length(), "form %s not found in document:\n%s", formSelector, html)
-
-	// Find the CSRF token
-	csrfToken, ok := form.Find("input[name=csrf_token]").Attr("value")
-	require.True(t, ok, "csrf_token not found in form %s", formSelector)
-
-	// Build form data
-	formData := url2.Values{}
-	formData.Add("csrf_token", csrfToken)
-	data := strings.NewReader(formData.Encode())
-
-	// Submit the form
-	resp, err := client.Post(baseURL+url, "application/x-www-form-urlencoded", data)
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	defer func(Body io.ReadCloser) {
-		err = Body.Close()
-		assert.NoError(t, err)
-	}(resp.Body)
-
-	// Parse the response
-	doc, err = goquery.NewDocumentFromReader(resp.Body)
-	require.NoError(t, err)
-	return doc
 }
--- cmd/web/testserver_test.go
+++ cmd/web/testserver_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/descope/virtualwebauthn"
+	"github.com/justinas/nosurf"
 	"github.com/myrjola/sheerluck/internal/errors"
 	"github.com/myrjola/sheerluck/internal/logging"
 	"github.com/stretchr/testify/assert"
@@ -10,6 +13,8 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	url2 "net/url"
+	"strings"
 	"testing"
 	"time"
 )
@@ -67,8 +72,15 @@ func testLookupEnv(key string) (string, bool) {
 	}
 }
 
+type testServer struct {
+	url           string
+	client        http.Client
+	rp            virtualwebauthn.RelyingParty
+	authenticator virtualwebauthn.Authenticator
+}
+
 // startTestServer starts the test server, waits for it to be ready, and return the server URL for testing.
-func startTestServer(t *testing.T, w io.Writer, lookupEnv func(string) (string, bool)) string {
+func startTestServer(t *testing.T, w io.Writer, lookupEnv func(string) (string, bool)) testServer {
 	t.Helper()
 	ctx, cancel := context.WithCancel(context.Background())
 
@@ -95,12 +107,177 @@ func startTestServer(t *testing.T, w io.Writer, lookupEnv func(string) (string, 
 	select {
 	case <-ctx.Done():
 		t.Fatal("server failed to start")
-		return ""
+		return testServer{} //nolint:exhaustruct // This is unreachable.
 	case addr := <-addrCh:
 		serverURL := fmt.Sprintf("http://%s", addr)
 		if err := waitForReady(ctx, fmt.Sprintf("%s/api/healthy", serverURL)); err != nil {
 			require.NoError(t, err)
 		}
-		return serverURL
+		jar, err := newUnsafeCookieJar()
+		require.NoError(t, err)
+		return testServer{
+			url:           serverURL,
+			client:        http.Client{Jar: jar},
+			rp:            virtualwebauthn.RelyingParty{Name: "Sheerluck", ID: "localhost", Origin: "http://localhost:0"},
+			authenticator: virtualwebauthn.NewAuthenticator(),
+		}
 	}
+}
+
+func (s *testServer) Client() http.Client {
+	return s.client
+}
+
+func (s *testServer) URL() string {
+	return s.url
+}
+
+// Get fetches a URL and returns the response.
+func (s *testServer) Get(t *testing.T, urlPath string) *http.Response {
+	t.Helper()
+	resp, err := s.client.Get(s.url + urlPath)
+	require.NoError(t, err)
+	return resp
+}
+
+// GetDoc fetches a URL and returns a goquery document.
+func (s *testServer) GetDoc(t *testing.T, urlPath string) *goquery.Document {
+	t.Helper()
+	resp := s.Get(t, urlPath)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func() {
+		err := resp.Body.Close()
+		require.NoError(t, err)
+	}()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	require.NoError(t, err)
+	return doc
+}
+
+// NewRequest creates a new HTTP request to the server.
+func (s *testServer) NewRequest(t *testing.T, method, urlPath string, body io.Reader) *http.Request {
+	t.Helper()
+	req, err := http.NewRequest(method, s.url+urlPath, body)
+	require.NoError(t, err)
+	return req
+}
+
+// Register registers a new WebAuthn credential with the server and returns the front page document.
+func (s *testServer) Register(t *testing.T) *goquery.Document {
+	doc := s.GetDoc(t, "/")
+
+	// Extract CSRF token from the form.
+	registrationStartURLPath := "/api/registration/start"
+	formSelector := fmt.Sprintf("form[action='%s']", registrationStartURLPath)
+	form := doc.Find(formSelector)
+	csrfToken, ok := form.Find("input[name=csrf_token]").Attr("value")
+	require.True(t, ok, "csrf_token not found in form %s", formSelector)
+
+	// Start Webauthn registration.
+	req := s.NewRequest(t, http.MethodPost, registrationStartURLPath, nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set(nosurf.HeaderName, csrfToken)
+	resp, err := s.client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	var bodyBytes []byte
+	bodyBytes, err = io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	attOpts, err := virtualwebauthn.ParseAttestationOptions(string(bodyBytes))
+	require.NoError(t, err)
+	credential := virtualwebauthn.NewCredential(virtualwebauthn.KeyTypeEC2)
+
+	// Finalise Webauthn registration.
+	attestationResponse := virtualwebauthn.CreateAttestationResponse(s.rp, s.authenticator, credential, *attOpts)
+	req = s.NewRequest(t, http.MethodPost, "/api/registration/finish", strings.NewReader(attestationResponse))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set(nosurf.HeaderName, csrfToken)
+	resp, err = s.client.Do(req)
+	require.NoError(t, err)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	// At this point, our credential is ready for logging in.
+	s.authenticator.AddCredential(credential)
+	// This option is needed for making Passkey login work.
+	s.authenticator.Options.UserHandle = []byte(attOpts.UserID)
+
+	return s.GetDoc(t, "/")
+}
+
+// Login logs in to the server given there is a registered WebAuthn credential and returns the front page document.
+func (s *testServer) Login(t *testing.T) *goquery.Document {
+	// Extract CSRF token from the form
+	loginStartURLPath := "/api/login/start"
+	formSelector := fmt.Sprintf("form[action='%s']", loginStartURLPath)
+	doc := s.GetDoc(t, "/")
+	form := doc.Find(formSelector)
+	csrfToken, ok := form.Find("input[name=csrf_token]").Attr("value")
+	require.True(t, ok, "csrf_token not found in form %s", formSelector)
+
+	req, err := http.NewRequest(http.MethodPost, s.url+loginStartURLPath, nil)
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set(nosurf.HeaderName, csrfToken)
+	var resp *http.Response
+	resp, err = s.client.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	var bodyBytes []byte
+	bodyBytes, err = io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	asOpts, err := virtualwebauthn.ParseAssertionOptions(string(bodyBytes))
+	require.NoError(t, err)
+	credential := s.authenticator.Credentials[0]
+	asResp := virtualwebauthn.CreateAssertionResponse(s.rp, s.authenticator, credential, *asOpts)
+	require.NoError(t, err)
+	req, err = http.NewRequest(http.MethodPost, s.url+"/api/login/finish", strings.NewReader(asResp))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set(nosurf.HeaderName, csrfToken)
+	resp, err = s.client.Do(req)
+	require.NoError(t, err)
+	err = resp.Body.Close()
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+
+	return s.GetDoc(t, "/")
+}
+
+// SubmitForm submits a form at formUrlPath with action formActionUrlPath and returns the response document.
+func (s *testServer) SubmitForm(t *testing.T, formURLPath string, formActionURLPath string) *goquery.Document {
+	doc := s.GetDoc(t, formURLPath)
+	html, err := doc.Html()
+	require.NoError(t, err)
+
+	// Extract CSRF token from the form.
+	formSelector := fmt.Sprintf("form[action='%s']", formActionURLPath)
+	form := doc.Find(formSelector)
+	require.Equal(t, 1, form.Length(), "form %s not found in document:\n%s", formSelector, html)
+	csrfToken, ok := form.Find("input[name=csrf_token]").Attr("value")
+	require.True(t, ok, "csrf_token not found in form %s", formSelector)
+
+	// Build form data
+	formData := url2.Values{}
+	formData.Add("csrf_token", csrfToken)
+	data := strings.NewReader(formData.Encode())
+
+	// Submit the form
+	resp, err := s.client.Post(s.url+formActionURLPath, "application/x-www-form-urlencoded", data)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
+	defer func(Body io.ReadCloser) {
+		err = Body.Close()
+		assert.NoError(t, err)
+	}(resp.Body)
+
+	// Parse the response
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	require.NoError(t, err)
+	return doc
 }
