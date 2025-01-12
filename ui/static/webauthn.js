addee5f8ed6cc5f6fa5adac3886c7ff65d7d9794
Martin Yrjölä <martin.yrjola@gmail.com>
2025-01-12 18:16:44 +0200

    Refactor webauthn code further

--- ui/static/webauthn.js
+++ ui/static/webauthn.js
@@ -28,111 +28,141 @@ function bufferEncode(value) {
 }
 
 /**
- * @param e {Event}
+ * Extracts CSRF token from a form in the HTTP headers format accepted by fetch.
+ * @param form {HTMLFormElement}
+ * @returns {Record<string, string>}
  */
-export async function registerUser(e) {
-  e.preventDefault()
-  const data = new FormData(e.target)
-  const headers = {'X-CSRF-Token': data.get('csrf_token')}
-  const registerStartURLPath = e.target.action
-  const resp = await fetch(registerStartURLPath, {method: "post", headers})
+function extractCsrfTokenHeaders(form) {
+  const data = new FormData(form)
+  return {'X-CSRF-Token': data.get('csrf_token').toString()}
+}
+
+/**
+ * Submits given form handling CSRF tokens and decoding JSON.
+ * @param form {HTMLFormElement}
+ * @returns {Promise<any>}
+ */
+async function submitForm(form) {
+  const headers = extractCsrfTokenHeaders(form)
+  const url = form.action
+  const resp = await fetch(url, {method: "post", headers})
 
   if (!resp.ok) {
-    throw new Error(`Failed to start registration!`);
+    throw new Error(`Failed to submit form!`);
   }
 
-  const credentialCreationOptions = await resp.json()
+  return resp.json()
+}
 
-  credentialCreationOptions.publicKey.challenge = bufferDecode(
-    credentialCreationOptions.publicKey.challenge,
-  );
-  credentialCreationOptions.publicKey.user.id = bufferDecode(
-    credentialCreationOptions.publicKey.user.id,
-  );
-  if (credentialCreationOptions.publicKey.excludeCredentials) {
-    for (
-      var i = 0;
-      i < credentialCreationOptions.publicKey.excludeCredentials.length;
-      i++
-    ) {
-      credentialCreationOptions.publicKey.excludeCredentials[i].id =
-        bufferDecode(
-          credentialCreationOptions.publicKey.excludeCredentials[i].id,
-        );
-    }
-  }
-  const credential = await navigator.credentials.create({
-    publicKey: credentialCreationOptions.publicKey,
-  });
+/**
+ * Creates Webauthn attestation response to be sent to finish registration endpoint.
+ * @param publicKey is the publicKey field in response from the start registration endpoint.
+ * @returns {Promise<string>}
+ */
+async function createAttestationResponse(publicKey) {
+  publicKey.challenge = bufferDecode(/** @type {string} */ publicKey.challenge);
+  publicKey.user.id = bufferDecode(/** @type {string} */ publicKey.user.id);
+  publicKey.excludeCredentials = publicKey.excludeCredentials?.map((excludedCredential) => ({
+    ...excludedCredential,
+    id: bufferDecode(excludedCredential.id),
+  }))
+  const credential = await navigator.credentials.create({ publicKey });
 
-  let attestationObject = credential.response.attestationObject;
-  let clientDataJSON = credential.response.clientDataJSON;
-  let rawId = credential.rawId;
+  const attestationObject = credential.response.attestationObject;
+  const clientDataJSON = credential.response.clientDataJSON;
+  const rawId = credential.rawId;
 
-  const finishResp = await fetch("/api/registration/finish", {
-    method: "post",
-    headers,
-    body: JSON.stringify({
-      id: credential.id,
-      rawId: bufferEncode(rawId),
-      type: credential.type,
-      response: {
-        attestationObject: bufferEncode(attestationObject),
-        clientDataJSON: bufferEncode(clientDataJSON),
-      },
-    }),
+  return JSON.stringify({
+    id: credential.id,
+    rawId: bufferEncode(rawId),
+    type: credential.type,
+    response: {
+      attestationObject: bufferEncode(attestationObject),
+      clientDataJSON: bufferEncode(clientDataJSON),
+    },
   })
+}
 
+/**
+ * Finishes registration with the server and reloads the page.
+ * @param attestationResponse is the payload sent to the finish registration endpoint.
+ * @param headers is the CSRF token headers to be sent with the request.
+ * @returns {Promise<void>}
+ */
+async function finishRegistration(attestationResponse, headers) {
+  const finishResp = await fetch("/api/registration/finish", { method: "post", headers, body: attestationResponse })
   if (!finishResp.ok) {
     throw new Error("Finishing registration failed!");
   }
-
+  // At this point, we assume the cookies are in place so that we can reload the page with the proper access.
   window.location.reload();
 }
 
 /**
+ * Registers a user using Webauthn.
+ * @param e {Event}
+ */
+export async function registerUser(e) {
+  try {
+    e.preventDefault()
+    const credentialCreationOptions = await submitForm(e.target)
+    const attestationResponse = await createAttestationResponse(credentialCreationOptions.publicKey)
+    await finishRegistration(attestationResponse, extractCsrfTokenHeaders(e.target))
+  } catch (err) {
+    console.error(err)
+    throw new Error("Registration failed!");
+  }
+}
+
+/**
+ * Creates Webauthn assertion response to be sent to finish login endpoint.
+ * @param publicKey is the publicKey field in response from the start login endpoint.
+ * @returns {Promise<string>}
+ */
+async function createAssertionResponse(publicKey) {
+  publicKey.challenge = bufferDecode(/** @type {string} */ publicKey.challenge);
+  const assertion = await navigator.credentials.get({ publicKey });
+  const {id, rawId, type, response: {authenticatorData, clientDataJSON, signature, userHandle}} = assertion;
+  return JSON.stringify({
+    id,
+    rawId: bufferEncode(rawId),
+    type,
+    response: {
+      authenticatorData: bufferEncode(authenticatorData),
+      clientDataJSON: bufferEncode(clientDataJSON),
+      signature: bufferEncode(signature),
+      userHandle: bufferEncode(userHandle),
+    },
+  })
+}
+
+/**
+ * Finishes login with the server and reloads the page.
+ * @param assertionResponse is the payload sent to the finish login endpoint.
+ * @param headers {Record<string, string>} is the HTTP headers for the request.
+ * @returns {Promise<void>}
+ */
+async function finishLogin(assertionResponse, headers) {
+  const finishResp = await fetch("/api/login/finish", { method: "post", headers, body: assertionResponse })
+  if (!finishResp.ok) {
+    throw new Error("Finishing login failed!");
+  }
+  // At this point, we assume the cookies are in place so that we can reload the page with the proper access.
+  window.location.reload();
+}
+
+/**
+ * Logs in a user using Webauthn.
  * @param e {Event}
  */
 export async function loginUser(e) {
   e.preventDefault()
-  const data = new FormData(e.target)
-  const headers = {'X-CSRF-Token': data.get('csrf_token')}
-  const loginStartURLPath = e.target.action
-  const resp = await fetch(loginStartURLPath, {method: "post", headers})
-  const credentialRequestOptions = await resp.json()
-
-  credentialRequestOptions.publicKey.challenge = bufferDecode(
-    credentialRequestOptions.publicKey.challenge,
-  );
-  const assertion = await navigator.credentials.get({
-    publicKey: credentialRequestOptions.publicKey,
-  });
-
-  let authData = assertion.response.authenticatorData;
-  let clientDataJSON = assertion.response.clientDataJSON;
-  let rawId = assertion.rawId;
-  let sig = assertion.response.signature;
-  let userHandle = assertion.response.userHandle;
-
-  const finishResp = await fetch("/api/login/finish", {
-    method: "post",
-    headers,
-    body: JSON.stringify({
-      id: assertion.id,
-      rawId: bufferEncode(rawId),
-      type: assertion.type,
-      response: {
-        authenticatorData: bufferEncode(authData),
-        clientDataJSON: bufferEncode(clientDataJSON),
-        signature: bufferEncode(sig),
-        userHandle: bufferEncode(userHandle),
-      },
-    }),
-  })
-
-  if (!finishResp.ok) {
-    throw new Error('Finishing login failed!');
+  try {
+    const credentialRequestOptions = await submitForm(e.target)
+    const assertionResponse = await createAssertionResponse(credentialRequestOptions.publicKey)
+    await finishLogin(assertionResponse, extractCsrfTokenHeaders(e.target))
+  } catch (err) {
+    console.error(err)
+    throw new Error("Login failed!");
   }
-
-  window.location.reload();
 }
